0ba885dc8dc425e249d9cb9d9814eda92d07df83
Kjake701 <k.jake701@hotmail.com>
2014-06-11 15:02:53 -0400

    test1

--- src/main/java/nick/main/Nick.java
+++ src/main/java/nick/main/Nick.java
@@ -9,7 +9,6 @@ import nick.util.Logging;
 import nick.util.NickMaker;
 
 public class Nick {
-	static final Logger logger = LogManager.getLogger(Nick.class.getName());
 	public static void main(String[] args) {
 			new NickMaker(0);
 		
